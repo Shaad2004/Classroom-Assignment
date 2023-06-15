5acbe76f2a34e158de3c1bfeb0302fade8c6514f
Shaad2004 <132331685+Shaad2004@users.noreply.github.com>
2023-06-15 23:39:57 +0530

    Create count vowels in string.cpp

---
+++ count vowels in string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string z ="shaad";
+    int d=0;
+    for (int j=0;j<=z.size();j++){ 
+       if (z[j]=='i' || z[j]=='a' || z[j]=='e' || z[j]=='o' || z[j]=='u'){
+        d++;
+    }}
+    cout<<d;
+    return 0;
+}
